b67a4a25658995ac561b63968b766c6e726c3ddd
baekCode <sosaer88@gmail.com>
2020-08-31 18:46:43 +0900

    feat : 리덕스 Provider

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import store, { history } from './store';
 import App from './components/App';
 import GlobalStyle from './styles/GlobalStyle';
 
 ReactDOM.render(
-  <>
+  <Provider store={store}>
     <GlobalStyle />
-    <App />
-  </>,
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
   document.getElementById('root'),
 );
