66f72de4531407b4952ed0e6b864a62d5708a14a
baekCode <sosaer88@gmail.com>
2020-08-31 15:26:34 +0900

    feat : 로그인 redux saga 구현

--- client/src/redux/saga/index.jsx
+++ client/src/redux/saga/index.jsx
@@ -1,5 +1,12 @@
-import { all } from 'redux-saga/effects';
+import axios from 'axios';
+import dotenv from 'dotenv';
+import { all, fork } from 'redux-saga/effects';
+import authSaga from './authSaga';
+
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL;
 
 export default function* rootSaga() {
-  yield all([]);
+  yield all([fork(authSaga)]);
 }
