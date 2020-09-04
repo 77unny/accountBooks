272d9bf04ab529731dcd760807b0f466cd364d11
baekCode <sosaer88@gmail.com>
2020-09-04 22:52:29 +0900

    feat : 초기접속시 로그인 상태 확인 로직 구현

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,6 +5,21 @@ import { ConnectedRouter } from 'connected-react-router';
 import store, { history } from './store';
 import App from './components/App';
 import GlobalStyle from './styles/GlobalStyle';
+import { USER_LOADING_REQUEST } from './redux/types';
+
+const loadUser = () => {
+  console.log(store.getState().auth);
+  try {
+    store.dispatch({
+      type: USER_LOADING_REQUEST,
+      payload: localStorage.getItem('token'),
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+loadUser();
 
 ReactDOM.render(
   <Provider store={store}>
